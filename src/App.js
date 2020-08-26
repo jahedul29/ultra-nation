d3a9d20570979e665bed3d90bd1b44ab2a30069d
jahedul29 <jahedulh1@gmail.com>
2020-08-27 05:58:39 +0600

    Data loaded

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.eu/rest/v2/all")
+      .then((res) => res.json())
+      .then((data) => {
+        setCountries(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  console.log(countries);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {countries.map((country) => (
+          <li>{country.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
